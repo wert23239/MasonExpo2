b81038eca757b25c2075120b706bac5949910786
wert23239 <lambe168@msu.edu>
2016-07-31 13:26:52 -0400

    Set up Site Manager and  Fixed Individual Site Site Manager is a constant sheet for all custom webpage info  Individual Site now has icon as well for the front page

---
+++ lib/Site/SiteManager.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 4/29/2016
+ * Time: 12:15 AM
+ */
+
+namespace Site;
+
+
+class SiteManager
+{
+
+
+    const CraftersDisplay="Crafters";
+    const CarDisplay="Car Show";
+    const MotorcycleDisplay="Motorcycle Show";
+    const TractorDisplay="Tractor Show";
+
+
+    const CraftersSlideShow=array("");
+    const CarSlideShow=array("");
+    const MotorcycleSlideShow=array("");
+    const TractorSlideShow=array("");
+
+    const CraftersInfo="";
+    const CarInfo="";
+    const MotorcycleInfo="";
+    const TractorInfo="";
+
+
+    const CraftersForum="";
+    const CarForum="";
+    const MotorcycleForum="";
+    const TractorForum="";
+    
+    const CraftersMap="";
+    const CarMap="";
+    const MotorcycleMap="";
+    const TractorMap="";
+
+
+    const CraftersIcon="";
+    const CarIcon="";
+    const MotorcycleIcon="";
+    const TractorIcon="";
+
+    const CraftersNum=0;
+    const CarNum=1;
+    const MotorcycleNum=2;
+    const TractorNum=3;
+    
+    const Total=4;
+
+
+
+    public function __construct($id)
+    {
+        $this->id=$id;
+        $this->initialize();
+       // var_dump($this->Code);
+        $this->type=$this->Code[$id]->getType();
+        $this->current=$this->Code[$id];
+        $this->find();
+    }
+
+    public function find(){
+            if($this->id!=0){
+                $this->previous=$this->Code[$this->id-1];
+            }
+            else{
+                $this->previous=$this->Code[self::Total-1];
+            }
+            if($this->id!=self::Total-1){
+                $this->next=$this->Code[$this->id+1];
+            }
+            else{
+                $this->next=$this->Code[0];
+            }
+    }
+
+    public function initialize(){
+         $this->Code=array(
+             self::CraftersNum => new IndividualSite(self::CraftersNum,self::CraftersDisplay,self::CraftersSlideShow,self::CraftersInfo,
+                 self::CraftersForum,self::CraftersMap,self::CraftersIcon) ,
+             self::CarNum => new IndividualSite(self::CarNum,self::CarDisplay,self::CarSlideShow,self::CarInfo,
+                 self::CarForum,self::CarMap,self::CarIcon) ,
+             self::MotorcycleNum => new IndividualSite(self::MotorcycleNum,self::MotorcycleDisplay,self::MotorcycleSlideShow,self::MotorcycleInfo,
+                 self::MotorcycleForum,self::MotorcycleMap,self::MotorcycleIcon) ,
+             self::TractorNum => new IndividualSite(self::TractorNum,self::TractorDisplay,self::TractorSlideShow,self::TractorInfo,
+                 self::TractorForum,self::TractorMap,self::TractorIcon) ,
+         );
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrevious()
+    {
+        return $this->previous;
+    }
+
+    public function getNext()
+    {
+        return $this->next;
+    }
+
+    public function getCurrent()
+    {
+        return $this->current;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+        $this->current=$this->Code[$id];
+        $this->find();
+
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    private $id=0;
+    private $type;
+    private $next;
+    private $previous;
+    private $current;
+    private $Code=array();
+}
