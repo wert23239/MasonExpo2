840ddee36c9bf3828b01d9a898f5c942f22cb7ac
Roy Perryman <perrym23@msu.edu>
2016-08-25 14:03:47 -0400

    Removed unnecessary css files and changes the layout of the eventview

--- EventDisplay.php
+++ EventDisplay.php
@@ -1,6 +1,6 @@
 <?php
 require 'lib/game.inc.php';
-$view= new Site\EventView($site);
+$view = new Site\EventView($site);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,13 +8,12 @@ $view= new Site\EventView($site);
 
 <body class="project">
 
-
 <?php echo $view->presentheader();
 ?>
-<div class="jumbotron CodeA">
+
 <?php
-   echo $view->presentDisplay();
+echo $view->presentDisplay();
 ?>
-</div>
+
 </body>
 </html>
--- lib/Site/EventView.php
+++ lib/Site/EventView.php
@@ -13,33 +13,43 @@ class EventView extends DynamicView
 
     public function presentDisplay()
     {
-        $Display=$this->title;
-        $id=$this->id;
-        $images=$this->image;
+        $Display = $this->title;
+        $id = $this->id;
+        $images = $this->image;
         shuffle($images);
-        $html="";
-        $html.="<div id='slideshow'>";
-        foreach($images as $image) {
-            $html.=<<<HTML
+
+
+        $html = "";
+
+        $html .= <<<HTML
+        
+    <div class="row">
+    <div class='col-md-4' id="page-header">
+    <h1><strong>$Display</strong></h1>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li><a href="page-post.php?id=$id&Info=1">Info</a></li><br/>
+                            <li><a href="page-post.php?id=$id&Forum=1">Sign-up</a></li><br/>
+                            <li><a href="page-post.php?id=$id&Map=1">Map</a></li>
+                        </ul>
+
+    </div>
+    </div>
+
+HTML;
+
+        $html .="<div class='col-md-8' id='page-slideshow'>";
+        $html .= "<div id='slideshow'>";
+        foreach ($images as $image) {
+            $html .= <<<HTML
             <div>
             <img src="$image" height="400px" width="600px">
             </div>
 HTML;
         }
-        $html.="</div>";
-        $html.=<<<HTML
-
-    <h1 class="CodeB"><strong>$Display</strong></h1>
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul class="nav navbar-nav">
-                            <li><a href="page-post.php?id=$id&Info=1">Info</a></li>
-                            <li><a href="page-post.php?id=$id&Forum=1">Sign-up</a></li>
-                            <li><a href="page-post.php?id=$id&Map=1">Map</a></li>
-                        </ul>
-
-    </div>
-
-HTML;
+        $html .= "</div>";
+        $html .= "</div>";
+        $html .= "</div>";
         return $html;
     }
 }
