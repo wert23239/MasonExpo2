ac1b5a68c4c34286cbb00b04b698e0fcaad56143
wert23239 <lambe168@msu.edu>
2016-09-11 21:34:32 -0400

    Seperated CSS
    Now adding headadditional() removes all css

--- lib/Site/DynamicView.php
+++ lib/Site/DynamicView.php
@@ -23,14 +23,31 @@ class DynamicView extends View
     }
     public function headadditional()
     {
+        return <<<HTML
+    <link href="style/navbar.css" rel="stylesheet">
+    <link href="style/forms.css" rel="stylesheet">
+    <link href="style/expos.css" rel="stylesheet">
+    <link href="style/landing-page.css" rel="stylesheet">
+HTML;
+    }
+
+    public function presenthead()
+    {
     $title=$this->title;
-    $html=<<<HTML
+    $html="";
+    $html.=<<<HTML
+    <head>
     <title>MasonExpo | $title</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Nothing quite like the Mason Expo.">
-    <link rel="stylesheet" href="style/landing-page.css">
+    <link href="node_modules/Bootstrap/css/bootstrap.min.css" rel="stylesheet">
+HTML;
+    $html.=$this->headadditional();
+    $html.=<<<HTML
     <script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="jslib/header.js"></script>
+    <script src="typed.js"></script>
 <script>
 
 
@@ -57,7 +74,7 @@ $(function(){
 
 		});
 </script>
-
+</head>
 HTML;
 
         return $html;
@@ -67,10 +84,6 @@ HTML;
         return parent::presentheader();
     }
 
-    public function presenthead()
-    {
-        return parent::presenthead();
-    }
 
     public function presentDisplay()
     {
--- lib/Site/View.php
+++ lib/Site/View.php
@@ -20,36 +20,6 @@ class View
 
     public function presenthead(){
         $html="";
-        $html.=<<<HTML
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-HTML;
-        $html.=$this->headadditional();
-        $html.="</head>";
-        $html.=<<<HTML
-    <!-- Bootstrap -->
-    <link href="node_modules/Bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="style/navbar.css" rel="stylesheet">
-    <link href="style/forms.css" rel="stylesheet">
-    <link href="style/expos.css" rel="stylesheet">
-    <link href="style/landing-page.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-    <link href="style/font-awesome.css" rel="stylesheet" type="text/css" />
-    <script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js"></script>
-    <script src="jslib/header.js"></script>
-    <script src="typed.js"></script>
- </head>
-HTML;
         return $html;
     }
 
